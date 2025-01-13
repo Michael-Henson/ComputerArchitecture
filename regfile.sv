0a7a6ba0e778196744fb0404ebe98b55abb75070
brisaac <123986868+brisaac@users.noreply.github.com>
2025-01-13 17:14:29 -0600

    Files from local computer

---
+++ regfile.sv
@@ -0,0 +1,17 @@
+module regfile (input logic         clk, 
+		input logic 	    we3, 
+		input logic [4:0]   ra1, ra2, wa3, 
+		input logic [31:0]  wd3, 
+		output logic [31:0] rd1, rd2);
+   
+   logic [31:0] rf[31:0];
+   
+   // three ported register file
+   // read two ports combinationally
+   // write third port on rising edge of clock
+   // register 0 hardwired to 0
+   always_ff @(posedge clk)
+	if (we3)
+	
+	
+endmodule // regfile
